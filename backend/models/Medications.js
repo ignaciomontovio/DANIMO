e3cca4eaa0d52a1e9af28eb574cbdab4cd2598ae
AgustinCocciardi <agustincocciardi@outlook.com>
2025-05-13 19:47:23 -0300

    Create Medications & EmergencyContacts

---
+++ backend/models/Medications.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const Medications = sequelize.define('Medications', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    startDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    endDate: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    dosage: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    freezeTableName: true, // ✅ No pluralizar automáticamente
+    timestamps: false // 👈 Esto evita que Sequelize use createdAt y updatedAt
+});
+
+sequelize.sync();
+
+module.exports = Medications;

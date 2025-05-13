785f63eb5e8c028473c3c626cda2637547c6b1ac
AgustinCocciardi <agustincocciardi@outlook.com>
2025-05-13 20:24:32 -0300

    Sumo clases al model

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,12 +1,19 @@
-const Users = require("./Users");
 const ActivityRegisters = require("./ActivityRegisters");
+const EmergencyContacts = require("./EmergencyContacts");
 const EmotionRegisters = require("./EmotionRegisters");
+const Medications = require("./Medications");
 const MoodAlternators = require("./MoodAlternators");
-
+const Professionals = require("./Professionals");
+const SleepRegisters = require("./SleepRegisters");
+const Users = require("./Users");
 
 module.exports = {
-    Users,
     ActivityRegisters,
+    EmergencyContacts,
     EmotionRegisters,
-    MoodAlternators
+    Medications,
+    MoodAlternators,
+    Professionals,
+    SleepRegisters,
+    Users,
 };

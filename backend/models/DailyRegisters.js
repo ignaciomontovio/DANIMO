836ae48e033644d3e1bfc2ad3ba2710c9562b84b
AgustinCocciardi <agustincocciardi@outlook.com>
2025-05-13 21:49:14 -0300

    model DailyRegisters

---
+++ backend/models/DailyRegisters.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./sequelizeConfig').init()
+
+const DailyRegisters = sequelize.define('DailyRegisters', {
+    id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    }
+}, {
+    freezeTableName: true, // ✅ No pluralizar automáticamente
+    timestamps: false // 👈 Esto evita que Sequelize use createdAt y updatedAt
+});
+
+module.exports = DailyRegisters;

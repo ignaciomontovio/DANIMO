5455bc9771edfb7f7d9f53f046b480a62b9737bf
AgustinCocciardi <agustincocciardi@outlook.com>
2025-05-20 21:16:20 -0300

    Insert de quotes en Node solo si no existen

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const app = require('./app');
 const { testConnection, syncDatabase } = require('./config/sync');
+const seedQuotesIfEmpty = require('./utils/seedQuotes');
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
     await testConnection();
     await syncDatabase(); // usa alter por defecto
+    await seedQuotesIfEmpty();
     console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 });

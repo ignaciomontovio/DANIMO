cfef8f7ab01fefd6e208a59d0965757818041db6
nachoria <ignaciomontovio@gmail.com>
2025-06-05 23:45:18 -0300

    deactivate tests

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,15 @@
 -- Eliminar la base de datos si existe (opcional y peligroso en producción)
-DROP DATABASE IF EXISTS DANIMO;
+DROP DATABASE IF EXISTS danimo;
 
 -- Crear base de datos DANIMO si no existe
-CREATE DATABASE IF NOT EXISTS DANIMO;
-USE DANIMO;
+CREATE DATABASE IF NOT EXISTS danimo;
+USE danimo;
 
 -- Crear usuario con contraseña (solo si no existe)
 CREATE USER IF NOT EXISTS 'grupo101'@'localhost' IDENTIFIED BY 'grupo101';
 
 -- Darle todos los privilegios sobre la base de datos DANIMO
-GRANT ALL PRIVILEGES ON DANIMO.* TO 'grupo101'@'localhost';
+GRANT ALL PRIVILEGES ON danimo.* TO 'grupo101'@'localhost';
 
 -- Aplicar cambios
 FLUSH PRIVILEGES;

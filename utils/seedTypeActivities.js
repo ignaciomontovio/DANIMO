0945290c71c6979c8a5b29a34ae7a48da2f3be40
AgustinCocciardi <agustincocciardi@outlook.com>
2025-06-13 19:26:09 -0300

    edito seed typeActivities

--- utils/seedTypeActivities.js
+++ utils/seedTypeActivities.js
@@ -1,23 +1,43 @@
 const TypeActivity = require('../models/TypeActivities'); // o la ruta correspondiente
 
 const initialActivities = [
+    // Hogar
     { name: 'Cocinar', category: 'Hogar' },
     { name: 'Limpiar', category: 'Hogar' },
-    { name: 'Lavar ropa', category: 'Hogar' },
-    { name: 'Leer un libro', category: 'Hobby' },
-    { name: 'Escuchar musica', category: 'Hobby' },
-    { name: 'Tocar un instrumento', category: 'Hobby' },
-    { name: 'Jugar videojuegos', category: 'Hobby' },
-    { name: 'Pintar o dibujar', category: 'Hobby' },
+    { name: 'Lavar', category: 'Hogar' },
+    { name: 'Ordenar', category: 'Hogar' },
+    { name: 'Comprar', category: 'Hogar' },
+    { name: 'Regar', category: 'Hogar' },
+    { name: 'Reparar', category: 'Hogar' },
+
+    // Hobby
+    { name: 'Leer', category: 'Hobby' },
+    { name: 'Musica', category: 'Hobby' },
+    { name: 'Instrumento', category: 'Hobby' },
+    { name: 'Videojuegos', category: 'Hobby' },
+    { name: 'Pintar', category: 'Hobby' },
+    { name: 'Dibujar', category: 'Hobby' },
     { name: 'Entrenar', category: 'Hobby' },
-    { name: 'Deporte', category: 'Hobby' },
+    { name: 'Bailar', category: 'Hobby' },
+    { name: 'Salida', category: 'Hobby' },
+
+    // Estudio
     { name: 'Estudiar', category: 'Estudio' },
-    { name: 'Clase', category: 'Estudio' },
-    { name: 'Tarea', category: 'Estudio' },
-    { name: 'Reunion', category: 'Trabajo' },
+    { name: 'Escribir', category: 'Estudio' },
+    { name: 'Investigar', category: 'Estudio' },
+    { name: 'Practicar', category: 'Estudio' },
+    { name: 'Resumir', category: 'Estudio' },
+
+    // Trabajo
+    { name: 'Reunirse', category: 'Trabajo' },
     { name: 'Viajar', category: 'Trabajo' },
     { name: 'Entregas', category: 'Trabajo' },
     { name: 'Informes', category: 'Trabajo' },
+    { name: 'Presentaciones', category: 'Trabajo' },
+    { name: 'Planear', category: 'Trabajo' },
+    { name: 'Supervisar', category: 'Trabajo' },
+    { name: 'Redactar', category: 'Trabajo' },
+    { name: 'Responder', category: 'Trabajo' }
 ];
 
 const seedTypeActivities = async () => {

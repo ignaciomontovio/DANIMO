f278d016a5138df89e41b32c1b7ed601df9835a1
nachoria <ignaciomontovio@gmail.com>
2025-06-15 18:57:32 -0300

    force EcmaScript6

--- models/relations.js
+++ models/relations.js
@@ -1,4 +1,4 @@
-module.exports = (models) => {
+const defineRelations = (models) => {
     const {
         Users,
         Medications,
@@ -156,3 +156,4 @@ module.exports = (models) => {
         as: "User",
     });
 };
+export default defineRelations;

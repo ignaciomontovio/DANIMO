5042a5f6a244ad291c5d7f46fe54d1fe47fae492
ignacio montovio <iganciomontovio@gmail.com>
2025-06-27 18:57:36 -0300

    No links default response.

--- services/messageIntention/messageIntentionService.js
+++ services/messageIntention/messageIntentionService.js
@@ -13,4 +13,6 @@ export function validateMessageIntention(message) {
     return { hasSuicideRisk, containsLinks, isBriefResponse, hasADateReference };
 }
 
-export const containsLinksResponse = 'No se aceptan enlaces en este chat.';
+export const containsLinksResponse = `En Danimo queremos cuidarte, por eso no aceptamos links en el chat. 
+Si necesitás compartir algo, contanos con tus palabras y vamos a hacer lo posible para ayudarte. 
+¡Gracias por entender y confiar en nosotros!`

6e52c3cc84539fd8fa17ce1f8ec1a89ebb6e2437
AgustinCocciardi <agustincocciardi@outlook.com>
2025-07-14 22:02:12 -0300

    edito funcion de normalizacion para que permita fechas con - o /

--- utils/textNormalize.js
+++ utils/textNormalize.js
@@ -1,4 +1,4 @@
 export function cleanMessage(str) {
-    return str.toLowerCase().replace(/[^\w\s]/g, '')
+    return str.toLowerCase().replace(/[^\w\s\/\-]/g, '')
         .normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
